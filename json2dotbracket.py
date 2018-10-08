47684c6dd52fdfd4a13e57144236b76e16c68997
Anton Petrov <apetrov@ebi.ac.uk>
2018-10-08 15:30:52 +0100

    Distinguish between nested and non-nested pairs

--- json2dotbracket.py
+++ json2dotbracket.py
@@ -4,15 +4,15 @@ import json
 import os
 
 
-json_database = '/Users/apetrov/Desktop/basepairs'
+json_database_all = '/Users/apetrov/Desktop/basepairs'
+json_database_nested = '/Users/apetrov/Desktop/basepairs-no-crossing'
 
 def parse_json(filename):
-    location = os.path.join(json_database, filename + '.json')
-    if not os.path.exists(location):
+    if not os.path.exists(filename):
         print('Does not exist')
         return ('', '')
 
-    with open(location, 'r') as f:
+    with open(filename, 'r') as f:
         data = json.load(f)
 
     sequence = data['sequence']
@@ -23,12 +23,36 @@ def parse_json(filename):
             structure[annotation['seq_id1']-1] = '('
             structure[annotation['seq_id2']-1] = ')'
 
+    return {
+        '1d': sequence,
+        '2d': structure,
+    }
+
+
+def generate_output(pdb_id, bp_all, bp_nested):
+    all = list(bp_all['2d'])
+    nested = list(bp_nested['2d'])
+    final = []
+    for i, character in enumerate(all):
+        if character in ['(', ')'] and nested[i] == '.':
+            if character == '(':
+                final.append('{')
+            elif character == ')':
+                final.append('}')
+        else:
+            final.append(character)
+
     output = """>{}
 {}
 {}
-""".format(filename, sequence, ''.join(structure))
+""".format(pdb_id, bp_all['1d'], ''.join(final))
     print(output)
-    return (sequence, structure)
 
 
-parse_json(sys.argv[1])
+pdb_id = sys.argv[1]
+filename = os.path.join(json_database_all, pdb_id + '.json')
+bp_all = parse_json(filename)
+filename = os.path.join(json_database_nested, pdb_id + '.json')
+bp_nested = parse_json(filename)
+
+generate_output(pdb_id, bp_all, bp_nested)
