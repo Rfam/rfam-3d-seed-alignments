7ef4cdd2be263b66847586c964d0e7af5edf89e8
Blake Sweeney <bsweeney@ebi.ac.uk>
2025-02-27 13:14:18 +0000

    Fixes for publishing data
    For some reason nextflow isn't publishing things properly. To fix this I
    have another script which will publish things correctly.

--- main.nf
+++ main.nf
@@ -100,20 +100,6 @@ process add_structure_info {
   """
 }
 
-process publish_results {
-  queue 'datamover'
-
-  input:
-  path(report)
-  path(alignments)
-
-  """
-  [ -d ${params.ftp}/.preview/3d/alignments ] || mkdir -p ${params.ftp}/.preview/3d/alignments
-  cp $report ${params.ftp}/.preview/3d
-  cp $alignments ${params.ftp}/.preview/3d/alignments
-  """
-}
-
 workflow add_3d {
   take:
     matches
@@ -160,14 +146,7 @@ workflow add_3d {
 workflow {
   main:
     Channel.fromPath("mapping.tsv") | add_3d
-    add_3d.out.alignments | collect | set { alignments }
-    publish_results(add_3d.out.report, alignments)
   publish:
     add_3d.out.report >> 'report'
     add_3d.out.alignments >> 'alignments'
-}
-
-output {
-  directory "$launchDir/results"
-  mode 'copy'
 }
--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --time=24:00:00                  # walltime
+#SBATCH --time=30:00                     # walltime
 #SBATCH --ntasks=1                       # number of tasks
 #SBATCH --cpus-per-task=1                # number of CPUs Per Task i.e if your code is multi-threaded
 #SBATCH --nodes=1                        # number of nodes
-#SBATCH -p standard                      # partition(s)
+#SBATCH -p datamover                     # partition(s)
 #SBATCH --mem=10G                        # memory per node
-#SBATCH -J "rfam_3d"                     # job name
-#SBATCH -o "rfam_3d.out"                 # job output file
-#SBATCH -e "rfam_3d.err"                 # job error file
+#SBATCH -J "rfam_3d_publish"             # job name
+#SBATCH -o "rfam_3d_publish.out"         # job output file
+#SBATCH -e "rfam_3d_publish.err"         # job error file
 #SBATCH --mail-user=bsweeney@ebi.ac.uk   # email address to message
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -21,6 +21,5 @@ IFS=$'\n\t'
 # pipelines.
 export NXF_OPTS='-Dnxf.pool.type=sync -Dnxf.pool.maxThreads=10000'
 
-module load nextflow
-nextflow run main.nf -ansi-log false -profile slurm -with-report
-sbatch publish.sh
+cp results/report/report.txt $RFAM_FTP/.preview/3d/
+cp results/alignments/*.sto $RFAM_FTP/.preview/3d/alignments/
